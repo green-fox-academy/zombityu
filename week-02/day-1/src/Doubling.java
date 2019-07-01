18e5f0b3c90e545de5a13bbba6091ec4ec20b38c
zombityu <vagasi.ityu@gmail.com>
2019-07-01 14:02:47 +0200

    Finished the first two function

---
+++ week-02/day-1/src/Doubling.java
@@ -0,0 +1,11 @@
+public class Doubling {
+    public static void main(String[] args) {
+        int baseNum =123;
+        doubling(baseNum);
+        System.out.println(doubling(baseNum));
+    }
+
+    private static int  doubling(int num) {
+     return num*2;
+    }
+}
---
+++ week-02/day-1/src/Greet.java
@@ -0,0 +1,10 @@
+public class Greet {
+    public static void main(String[] args) {
+        String al = "Green Fox";
+        greet(al);
+    }
+
+    private static void greet(String al) {
+        System.out.println("Greetings dear, "+al);
+    }
+}
