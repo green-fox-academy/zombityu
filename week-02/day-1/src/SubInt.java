8b9b3ac31385ad97db65ac412c1a55bd74a0d74c
zombityu <vagasi.ityu@gmail.com>
2019-07-01 17:52:04 +0200

    Finished the basic function

---
+++ week-02/day-1/src/SubInt.java
@@ -0,0 +1,40 @@
+public class SubInt {
+    public static void main(String[] args) {
+        subInt(1, new int [] {1, 11, 34, 52, 61});
+        System.out.println(subInt(1, new int [] {1, 11, 34, 52, 61}));
+    }
+
+    private static int []  subInt(int a, int array []) {
+         int count=0;
+
+        for (int i = 0; i < array.length; i++) {
+            if () {
+                count++;
+                }
+            }
+        int[] temp = new int [count];
+        for (int i = 0; i <array.length ; i++) {
+            if (array[i]==a){
+                temp[i]=i;
+            }
+
+        }
+
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] == a) {
+//                temp[tempIndex]=i;
+//                tempIndex++;
+
+
+
+
+        return temp;
+
+        }
+
+
+    }
+
+
+
---
+++ week-02/day-1/src/Unique.java
@@ -0,0 +1,13 @@
+public class Unique {
+    public static void main(String[] args) {
+//        unique();
+    }
+
+//    private static int [] unique(int array []) {
+//
+//        for (int i = 0; i <array.length; i++) {
+
+//        }
+//
+//    }
+}
