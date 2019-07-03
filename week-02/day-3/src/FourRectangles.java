fbd2df51f290c38e7c8fed337730f98c140bec47
zombityu <vagasi.ityu@gmail.com>
2019-07-03 13:53:01 +0200

    Finished square position

---
+++ week-02/day-3/src/FourRectangles.java
@@ -0,0 +1,51 @@
+import org.w3c.dom.css.RGBColor;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+
+        Color[] color = {Color.black, Color.red, Color.cyan, Color.orange};
+
+
+        for (int i = 0; i <4; i++) {
+            graphics.setColor(color[i]);
+            graphics.fillRect(50, 50, 200-(i*20),100-(i*20));
+        }
+
+
+
+    }
+
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
