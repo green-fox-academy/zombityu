d5cecae973fdd4a122338e4f7cd49dce62df8dd5
zombityu <vagasi.ityu@gmail.com>
2019-07-05 00:16:38 +0200

    Finished product database, and improved it

--- week-02/day-2/src/ProductDatabase.java
+++ week-02/day-2/src/ProductDatabase.java
@@ -8,9 +8,50 @@ public class ProductDatabase {
         System.out.println("The price of the Fish: "+howMuchIsTheFish(list));
         System.out.println("The most expensive product: "+theMostExpensiveProduct(list));
         System.out.println("The average of the price: "+averagePrice(list));
+        System.out.println(priceBelow(list)+" product is below 300.");
+        System.out.println(isThereAnything(list));
+        System.out.println("The cheapest product is: "+cheapestProduct(list));
 
 
 
+    }
+
+    private static String cheapestProduct(HashMap<String, Integer> list) {
+        int min=(int)averagePrice(list);
+        String cheapProduct="";
+        for (Map.Entry<String,Integer> entry:list.entrySet()) {
+            if (entry.getValue()<min){
+                min=entry.getValue();
+            }
+        }
+        for (Map.Entry<String,Integer> entry:list.entrySet()) {
+            if (entry.getValue().equals(min)){
+                cheapProduct=entry.getKey()+" The price is: "+entry.getValue();
+            }
+        }
+        return cheapProduct ;
+    }
+
+
+    private static String isThereAnything(HashMap<String, Integer> list) {
+        String answer = "No you haven't got enough money.";
+        for (Map.Entry<String,Integer> entry:list.entrySet()) {
+            if (entry.getValue()<125){
+                answer="Yes you have enough money. You can buy some "+entry.getKey();
+            }
+        }
+
+        return  answer;
+    }
+
+    private static int priceBelow(HashMap<String, Integer> list) {
+        int counter=0;
+        for (int below:list.values()) {
+            if (below<300){
+                counter++;
+            }
+        }
+        return counter;
     }
 
     private static float averagePrice(HashMap<String, Integer> list) {
