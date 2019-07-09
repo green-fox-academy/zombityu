0b139095781199b6babc9cf507efa5a8b2c4b4b3
zombityu <vagasi.ityu@gmail.com>
2019-07-09 21:59:40 +0200

    Modified logs

--- week-03/day-2/src/Logs.java
+++ week-03/day-2/src/Logs.java
@@ -9,26 +9,18 @@ import java.util.regex.Pattern;
 public class Logs {
     public static void main(String[] args) {
 
-        String file = "D:\\GreenFox\\zombityu\\week-03\\day-2\\log.txt";
 
-        ipAdresses(file);
-        System.out.println(getPostRatio(file));
-
-
-
-
+        Path filePath = Paths.get("D:\\GreenFox\\zombityu\\week-03\\day-2\\log.txt");
+        ipAdresses(filePath);
+        System.out.println(getPostRatio(filePath));
     }
 
-    private static double getPostRatio(String file) {
+    private static double getPostRatio(Path filePath) {
 
         int get = 0;
         double ratio = 0;
-
         try {
-            Path filePath = Paths.get(file);
             List<String> logList = Files.readAllLines(filePath);
-            System.out.println(logList);
-
             for (int i = 0; i <logList.size(); i++) {
                 if (logList.get(i).contains("GET")){
                     get++;
@@ -40,34 +32,28 @@ public class Logs {
         ratio = (double)get *100 / 500;
         return ratio;
 
-
     }
 
-
-
-
-    private static void ipAdresses(String file) {
+    private static void ipAdresses(Path filePath) {
         try {
-            Path filePath = Paths.get(file);
             List<String> logList = Files.readAllLines(filePath);
+            ArrayList<String> ipList = new ArrayList<>();
 
-            ArrayList<String> ips = new ArrayList<>();
-
-
-            for (int i = 0; i < logList.size(); i++) {
-                String pattern = "(\\\\d{1,3}[.]\\\\d{1,3}[.]\\\\d{1,3}[.]\\\\d{1,3})";
+            for (int i = 0; i <logList.size() ; i++) {
+                String pattern = "(\\d{1,3}[.]\\d{1,3}[.]\\d{1,3}[.]\\d{1,3})";
 
                 Pattern p = Pattern.compile(pattern);
 
                 Matcher m = p.matcher(logList.get(i));
-
-                if (m.find()){
-                    ips.add(m.group());
+                if (m.find()) {
+                    ipList.add(m.group());
                 }
-
             }
-            Set<String> set = new HashSet<>(ips);
+
+            Set<String> set = new HashSet<>(ipList);
             System.out.println(set);
+            System.out.println(set.size());
+
 
         } catch (IOException e) {
             System.out.println("Error");
--- week-03/day-2/src/TicTacToe.java
+++ week-03/day-2/src/TicTacToe.java
@@ -1,45 +1,45 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-public class TicTacToe {
-    public static void main(String[] args) {
-
-//        System.out.println(ticTacResult("win-o.txt"));
-        System.out.println(ticTacResult("win-x.txt"));
-//        ticTacResult("draw.txt");
-
-    }
-
-    private static String ticTacResult(String win) {
-//        ArrayList<Character> charList = new ArrayList<>();
-        int [][] matrix = new int [3][3];
-        String winO = "";
-        try {
-        Path filePath = Paths.get(win);
-        List<String> list = Files.readAllLines(filePath);
-            for (int i = 0; i <list.size() ; i++) {
-                for (int j = 0; j <list.size(); j++) {
-                    matrix[i][j] = list;
-
-                }
-                }
-////-------> First Line <-------
-//            for (int i = 0; i < 3; i++) {
-//                if (charList.get(i*3) == (charList.get(i*3)+3) && charList.get(i*3) == charList.get(+3)+6);
-//                    winO = charList.get(0).toString();
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class TicTacToe {
+//    public static void main(String[] args) {
+//
+////        System.out.println(ticTacResult("win-o.txt"));
+//        System.out.println(ticTacResult("win-x.txt"));
+////        ticTacResult("draw.txt");
+//
+//    }
+//
+//    private static String ticTacResult(String win) {
+////        ArrayList<Character> charList = new ArrayList<>();
+//        int [][] matrix = new int [3][3];
+//        String winO = "";
+//        try {
+//        Path filePath = Paths.get(win);
+//        List<String> list = Files.readAllLines(filePath);
+//            for (int i = 0; i <list.size() ; i++) {
+//                for (int j = 0; j <list.size(); j++) {
+//                    matrix[i][j] = list;
+//
 //                }
-////-------->
-//            for (int i = 0; i <3; i++) {
+//                }
+//////-------> First Line <-------
+////            for (int i = 0; i < 3; i++) {
+////                if (charList.get(i*3) == (charList.get(i*3)+3) && charList.get(i*3) == charList.get(+3)+6);
+////                    winO = charList.get(0).toString();
+////                }
+//////-------->
+////            for (int i = 0; i <3; i++) {
+////
+////            }
+////
+//        } catch (Exception e){
 //
-//            }
+//        }
+//        return winO;
+//    }
 //
-        } catch (Exception e){
-
-        }
-        return winO;
-    }
-
-}
+//}
